0123c412c720f443f5bf474c27e38fe5b2fccdce
Mattia Oldani <mattiaoldani6@gmail.com>
2023-05-04 18:08:28 +0200

    Sistemati parser e .gitignore

--- test/parser.py
+++ test/parser.py
@@ -1,18 +1,33 @@
 import argparse
+import os
 import serial
+from serial import SerialException
 
 
-def main(name: str) -> None:
-    s = serial.Serial("COM5", 9600)
-    
-    with open(f"{name}.csv", "a") as f:
+def main(filename: str, port: str) -> None:
+    while True:
+        try:
+            s = serial.Serial(port, 9600)
+            break
+        except SerialException:
+            port = input("Porta errata, inserisci la porta corretta: ")
+
+    files = [file.split(".")[0] for file in os.listdir()]
+
+    while filename not in files:
+        filename = input("Nome file errato, inserisci il nome corretto: ")
+
+    with open(f"{filename}.csv", "a") as f:
         for i in range(1000):
             f.write(f"{s.readline().strip().decode()}\n")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("name")
+    
+    parser.add_argument("filename")
+    parser.add_argument("port")
+    
     args = parser.parse_args()
 
-    main(args.name)
+    main(args.filename, args.port)
